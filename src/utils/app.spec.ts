6835e7355c80883300966cc134f36f20f8919c5a
jun233 <1227491415@qq.com>
2022-07-07 23:38:01 +0800

    修改测试名称

--- src/utils/app.spec.ts
+++ src/utils/app.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
 import { longestConsecutive } from './number'
-test('测试安装', () => {
+test('数字连续的最长序列', () => {
     expect(longestConsecutive([1,2,6,7,3])).toBe(3)
 })
